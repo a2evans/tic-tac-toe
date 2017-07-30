9ce23d80442ef124e390e92a69ece220fc28664c
a2evans <andrewevans7396@gmail.com>
2017-07-29 17:41:46 -0700

    added legends and first user input selections for mode.

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,8 +1,13 @@
 # tic-tac_toe project                                                           Andrew Evans
 
 print("Welcome to tic-tac-toe!")
+legend = "['7', '8', '9']\n['4', '5', '6']\n['1', '2', '3']"
+
+# dictionary for game input states
+inputDict = {1 : ,}
+
 gameState = [[0 for y in range(3)] for x in range(3)]
-print(gameState)
+# print(gameState)
 
 # function to print the board
 def print_game_state(gameStateP):
@@ -20,5 +25,32 @@ def print_game_state(gameStateP):
     for i in range(3):
         print(gameStateTemp[i])
 
+# function for determining mode
+def modeParse(mode):
+    if mode == 1:
+        return 5
+    elif mode == 2:
+        return 4
+    else:
+        print("please choose either 1 or 2 for the mode")
+        return -1
 
+
+# functions for the user's possible inputs
+
+# Initial Board
 print_game_state(gameState)
+
+# determine number of user inputs
+mode = input("Would you like to go first(enter 1) or second(enter 2)?\n")
+print(mode)
+mFlag = modeParse(int(mode))
+# print("please select an appropriate mode")
+print(legend)
+# print(mFlag)
+for j in range(mFlag):
+    print(legend)
+    selection = input("Where would you like to mark?\n")
+
+
+
