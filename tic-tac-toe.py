88e4302ea3717c35842ed59bcd569f52c0c7b326
a2evans <andrewevans7396@gmail.com>
2017-07-29 19:44:24 -0700

    Finished basic(random) ai moves

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -12,7 +12,7 @@ gameState = [[' ' for y in range(3)] for x in range(3)]
 
 # function for checking if the selection is a non-open state
 
-# function for choosing "ai(not yet)" moves
+
 
 # function to print the board
 def print_game_state(gameStateP):
@@ -106,26 +106,32 @@ stateChangeDict = {1: first, 2: second, 3: third, 4: fourth, 5: fifth, 6: sixth,
 # Initial Board
 print_game_state(gameState)
 
+
+# function for choosing "ai(not yet)" moves
+def anti_player_move():
+    move = random.choice(openStates)
+    stateChangeDict[move](1)
+    openStates.remove(move)
+    print("Your opponent has chosen: ", move)
+
+
 # determine number of user inputs
 mode = input("Would you like to go first(enter 1) or second(enter 2)?\n")
 mFlag = mode_parse(int(mode))
 
 # check to see if comp should go first
 if mode == '2':
-    # have the comp make a turn
-    firstMove = random.randint(1, 9)
-    print("first move is: ", firstMove)
-    stateChangeDict[firstMove](1)
-    # print_game_state(gameState)
-    openStates.remove(firstMove)
+    anti_player_move()
 
 for j in range(mFlag):
     print(legend)
     print("")
     selection = input("Where would you like to mark?\n")
+    # verify the selection
     stateChangeDict[int(selection)](0)
     openStates.remove(int(selection))
     print_game_state(gameState)
     print("")
+    anti_player_move()
 
 
